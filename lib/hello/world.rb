38d8312ec4a0cae85a3b5b2849fbc646afa20e64
wrlpeake <wrlpeake@gmail.com>
2022-10-05 11:52:48 +0100

    Initial Commit

---
+++ lib/hello/world.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "world/version"
+
+module Hello
+  module World
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/hello/world/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Hello
+  module World
+    VERSION = "0.1.0"
+  end
+end
